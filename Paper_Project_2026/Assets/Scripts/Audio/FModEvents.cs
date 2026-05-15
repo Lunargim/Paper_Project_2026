f9a2fc932a4d0e5dc55f978339f1889c78eb8c47
Lunargim <loryluna2001@gmail.com>
2026-05-15 19:20:54 +0200

    add new references to different footsteps for dif surface

--- Paper_Project_2026/Assets/Scripts/Audio/FModEvents.cs
+++ Paper_Project_2026/Assets/Scripts/Audio/FModEvents.cs
@@ -12,9 +12,17 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference music { get; private set; }
     
     [field: Header("Player")] 
+    
+    [field: Header("Player/Footspeps")]
+    
+    [field: SerializeField] public EventReference p_Footsteps_Wood {get; private set;}
+    [field: SerializeField] public EventReference p_Footsteps_Rock {get; private set;}
+    [field: SerializeField] public EventReference p_Footsteps_Grass {get; private set;}
+    [field: SerializeField] public EventReference p_Footsteps_Dirt {get; private set;}
+    
+    [field: Header("Player/Jump")]
     [field: SerializeField] public EventReference jump {get; private set;}
     [field: SerializeField] public EventReference jumpLanding {get; private set;}
-    [field: SerializeField] public EventReference playerFootsteps {get; private set;}
 
     public void Awake()
     {
