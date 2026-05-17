fee736591ddc91df61951cbc879c73b106d2fbae
Lunargim <loryluna2001@gmail.com>
2026-05-17 12:14:07 +0200

    refactor weather script to change with changing phase

--- Paper_Project_2026/Assets/Scripts/Weather/WeatherManager.cs
+++ Paper_Project_2026/Assets/Scripts/Weather/WeatherManager.cs
@@ -13,8 +13,11 @@ public class WeatherManager : MonoBehaviour
     [SerializeField] private GameObject _rainBox;
 
     private Color _dayLightColor;
+    
+    private GamePhase _currentPhase;
     public void Awake()
     {
+        _currentPhase = GamePhase.Sun;
         _dayLightColor = _directionalLight.color;
         _rainBox.SetActive(false);
     }
@@ -23,21 +26,46 @@ public class WeatherManager : MonoBehaviour
     {
         if (Keyboard.current.pKey.wasPressedThisFrame)
         {
-            if (_directionalLight.color == _rainDayLightColor)
+            if (_currentPhase == GamePhase.Sun)
             {
-                RenderSettings.skybox = _skyboxSun;
-                _directionalLight.color = _dayLightColor;
-                _rainBox.SetActive(false);
+                PhaseManager.instance.ChangeGamePhase(GamePhase.Rain);
+                _currentPhase = GamePhase.Rain;
             }
             else
             {
-                RenderSettings.skybox = _skyboxRain;
-                _directionalLight.color = _rainDayLightColor;
-                _rainBox.SetActive(true);
+                PhaseManager.instance.ChangeGamePhase(GamePhase.Sun);
+                _currentPhase = GamePhase.Sun;
             }
         }
 
     }
+    
+    private void ChangeWeather(GamePhase gamePhase)
+    {
+        if (gamePhase ==  GamePhase.Sun)
+        {
+            RenderSettings.skybox = _skyboxSun;
+            _directionalLight.color = _dayLightColor;
+            _rainBox.SetActive(false);
+        }
+        else
+        {
+            RenderSettings.skybox = _skyboxRain;
+            _directionalLight.color = _rainDayLightColor;
+            _rainBox.SetActive(true);
+        }
+    }
+
+    public void OnEnable()
+    {
+        PhaseManager.OnGamePhaseChange += ChangeWeather;
+    }
+
+    public void OnDisable()
+    {
+        PhaseManager.OnGamePhaseChange -= ChangeWeather;
+    }
+    
     
     //SAREBBE DA FARE A PHASE RAIN PHASE E SUN PHASE UN PO COME NEL GIOCO DI TEAMOWRK.
     
